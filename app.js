bf16928d1091fb33962c9ff3cbdaab9d02fa6c5d
asrujana <85404548+asrujana@users.noreply.github.com>
2023-03-17 00:57:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 function talk(){
 
 var know = {
-"Who are you" : "Hello, Codewith_random here ",
-"How are you" : "Good :)",
-"What can i do for you" : "Please Give us A Follow & Like.",
-"Your followers" : "I have my family of 5000 members, i don't have follower ,have supportive Famiy ",
-"ok" : "Thank You So Much",
 
+"How are you" : "Good :)",
+"ok" : "Thank You So Much",
 "Bye" : "Okay! Will meet soon..",
 "schemes":"related to education, Economic, health care",
 "education":"schemes are Eklavya Model Residential Schools, National Fellowship,Scholarship for Higher Education"
 "economic":"Schemes are Tribal Sub Plan (TSP),Pradhan Mantri Van Dhan Yojana, Institutional Support For Development And Marketing Of Tribal Products",
-"health care":"Schemes are Tribal Sub Plan (TSP), Pradhan Mantri Van Dhan Yojana, Institutional Support For Development And Marketing Of Tribal Products"
-	
+"health care":"Schemes are Tribal Sub Plan (TSP), Pradhan Mantri Van Dhan Yojana, Institutional Support For Development And Marketing Of Tribal Products",
+"trible welfare":"The government has implemented various policies and programs aimed at promoting tribal welfare"	
 };
 
 
